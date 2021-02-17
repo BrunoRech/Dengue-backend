c4638d0af4453c2b76663ab8a19d6451d23aad6a
unknown <bruno.rech@vexta.com.br>
2021-02-17 20:30:46 -0300

    refactor(sessao-controller): retorna o id junto com o token no login

--- src/controllers/Sessao/index.js
+++ src/controllers/Sessao/index.js
@@ -5,13 +5,13 @@ module.exports = {
     const { email } = req.body;
     const agente = await Agente.findOne({ where: { email } });
     const token = await agente.gerarToken();
-    return res.json({ token });
+    return res.json({ token, id: agente.id });
   },
 
   async gerarSessaoCoordenador(req, res) {
     const { email } = req.body;
     const coordenador = await Coordenador.findOne({ where: { email } });
     const token = await coordenador.gerarToken();
-    return res.json({ token });
+    return res.json({ token, id: coordenador.id });
   },
 };
